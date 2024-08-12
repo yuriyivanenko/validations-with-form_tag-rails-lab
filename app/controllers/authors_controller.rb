ad5583e602de4fbeca7f5de1d5187d375d59b910
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-12 14:54:12 -0400

    I hated this lab. So many small stupid errors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,14 +3,33 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def new
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
+  def update
+    @author = Author.find(params[:id])
 
-    redirect_to author_path(@author)
+    if @author.update(post_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+
+  end
+
+  def create
+    @author = Author.new(author_params)
+  
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new()
+  end
+
+  def create
+    @post = Post.new(post_params)
+  
+    if @post.save
+      redirect_to author_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
